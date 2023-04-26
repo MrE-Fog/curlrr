39550a5d0778d12dc5c2e8e4a573712ae3c7b97b
Daniel Stenberg <daniel@haxx.se>
2023-04-26 23:27:04 +0200

    urlapi: make internal function start with Curl_
    Curl_url_set_authority() it is.
    
    Follow-up to acd82c8bfd
    
    Closes #11035

--- lib/urlapi-int.h
+++ lib/urlapi-int.h
@@ -29,7 +29,6 @@ size_t Curl_is_absolute_url(const char *url, char *buf, size_t buflen,
                             bool guess_scheme);
 
 CURLUcode Curl_url_set_authority(CURLU *u, const char *authority,
-CURLUcode curl_url_set_authority(CURLU *u, const char *authority,
                                  unsigned int flags);
 
 #ifdef DEBUGBUILD
